0e18b5733dc73bbfe02974e11aec47427651c7a7
unknown <1706juliareis@gmail.com>
2023-06-10 02:55:32 -0300

    tcc final

--- FRONT/src/Teacher.js
+++ FRONT/src/Teacher.js
@@ -5,9 +5,9 @@ import axios from 'axios';
 import {Modal, ModalBody, ModalFooter, ModalHeader} from 'reactstrap';
 import logoCadastro from './assets/cadastro.png';
 
-function App() {
+function Teacher() {
 
-  const baseUrl="https://localhost:44380/api/Aluno";
+  const baseUrl="https://localhost:44380/api";
   const [data, setData]=useState([]);
   const [updateData, setUpdateData] = useState(true);
 
@@ -15,23 +15,21 @@ function App() {
   const [modalIncluir, setModalIncluir]=useState(false);
   const [modalExcluir, setModalExcluir]=useState(false);
 
-  const [alunoSelecionado, setAlunoSelecionado]=useState({
+  const [professorSelecionado, setProfessorSelecionado]=useState({
     id:'',
-    nomeAluno:'',
-    idade:'',
-    nomeResponsavel:'',
-    rg:'',
-    cpf:'',
-    dtentrada: new Date()
+    nomeProfessor:'',
+    atvResponsavel:'',
+
+    dtEntrada: new Date()
   })
 
   const handleChange=e=>{
     const {name, value}=e.target;
-    setAlunoSelecionado({
-      ...alunoSelecionado, 
+    setProfessorSelecionado({
+      ...professorSelecionado, 
       [name]: value
     });
-    console.log(alunoSelecionado);
+    console.log(professorSelecionado);
   }
 
   //-----modal controle do estado 
@@ -48,7 +46,7 @@ function App() {
   }
 
   const pedidoGet=async()=>{
-    await axios.get("https://localhost:44380/api/Aluno/todosalunos")
+    await axios.get("https://localhost:44380/api/TodosProfessores")
     .then(response=>{
      setData(response.data);
      setUpdateData(true)
@@ -59,9 +57,8 @@ function App() {
 }
 
   const pedidoPost=async()=>{
-    delete alunoSelecionado.id;
-    alunoSelecionado.idade=parseInt(alunoSelecionado.idade);
-      await axios.post(baseUrl, alunoSelecionado)
+    delete professorSelecionado.id;
+      await axios.post(baseUrl,professorSelecionado)
     .then(response=>{
       setData(data.concat(response.data));      
       setUpdateData(true)
@@ -72,19 +69,15 @@ function App() {
   }
 
   const pedidoPut=async()=>{
-    alunoSelecionado.idade=parseInt(alunoSelecionado.idade);
-    await axios.put("https://localhost:44380/api/Aluno?id="+alunoSelecionado.id, alunoSelecionado)
+    await axios.put("https://localhost:44380/api?id="+professorSelecionado.id, professorSelecionado)
     .then(response=>{
       var resposta=response.data;
       var dadosAuxiliar=data;
       //eslint-disable-next-line
-      dadosAuxiliar.map(aluno=>{
-        if(aluno.id===alunoSelecionado.id){
-          aluno.nomeAluno=resposta.nomeAluno;
-          aluno.idade=resposta.idade;
-          aluno.nomeResponsavel=resposta.nomeResponsavel;
-          aluno.rg=resposta.rg;
-          aluno.cpf=resposta.cpf;          
+      dadosAuxiliar.map(professor=>{
+        if(professor.id===professorSelecionado.id){
+          professor.nomeprofessor=resposta.nomeprofessor;
+          professor.atvResponsavel = resposta.atvResponsavel;   
         }
       });
       abrirFecharModalEditar();
@@ -96,9 +89,9 @@ function App() {
   }
 
   const pedidoDelete=async()=>{
-    await axios.delete(baseUrl+"/"+alunoSelecionado.id)
+    await axios.delete(baseUrl+"/"+professorSelecionado.id)
     .then(response=>{
-     setData(data.filter(aluno=>aluno.id!==response.data));
+     setData(data.filter(professor=>professor.id!==response.data));
       setUpdateData(true)
       abrirFecharModalExcluir();
     }).catch(error=>{
@@ -106,8 +99,8 @@ function App() {
     })
   }
 
-  const selecionarAluno=(aluno, caso)=>{
-    setAlunoSelecionado(aluno);
+  const selecionarProfessor=(professor, caso)=>{
+    setProfessorSelecionado(professor);
       (caso==="Editar")?
         abrirFecharModalEditar(): abrirFecharModalExcluir();
   }
@@ -124,34 +117,29 @@ function App() {
        <br/>
        <h2>Centro Cultural Relicário</h2>
        <br/><br/>
-       <h3>Cadastro de Alunos</h3>
+       <h3>Cadastro de Professores</h3>
       <header>
         <img src={logoCadastro} alt="Cadastro" />
-        <button onClick={()=>abrirFecharModalIncluir()} className="btn btn-success">Incluir Novo Aluno</button>
+        <button onClick={()=>abrirFecharModalIncluir()} className="btn btn-success">Incluir Novo Professor</button>
        </header>
       <table className="table table-bordered" >
         <thead>
           <tr>
             <th>Id</th>
-            <th>Nome do Aluno</th>
-            <th>Nome Responsável</th>
-            <th>Idade</th>
-            <th>RG</th>
-            <th>CPF</th>
+            <th>Nome do Professor</th>          
+            <th>Atividade responsável</th>
           </tr>
         </thead>
         <tbody>
-          {data.map(aluno=>(
-            <tr key={aluno.id}>
-              <td>{aluno.id}</td>
-              <td>{aluno.nomeAluno}</td>
-              <td>{aluno.nomeResponsavel}</td>
-              <td>{aluno.idade}</td>
-              <td>{aluno.rg}</td>
-              <td>{aluno.cpf}</td>
+          {data.map(professor=>(
+            <tr key={professor.id}>
+              <td>{professor.id}</td>
+              <td>{professor.nomeProfessor}</td>
+              <td>{professor.atvResponsavel}</td>
+             
               <td>
-              <button className="btn btn-primary" onClick={()=>selecionarAluno(aluno, "Editar")}>Editar</button> {"  "}
-              <button className="btn btn-danger" onClick={()=>selecionarAluno(aluno, "Excluir")}>Excluir</button>
+              <button className="btn btn-primary" onClick={()=>selecionarProfessor(professor, "Editar")}>Editar</button> {"  "}
+              <button className="btn btn-danger" onClick={()=>selecionarProfessor(professor, "Excluir")}>Excluir</button>
               </td>
               </tr>
           ))}
@@ -159,29 +147,18 @@ function App() {
       </table>
       
       <Modal isOpen={modalIncluir}>
-      <ModalHeader>Incluir Alunos</ModalHeader>
+      <ModalHeader>Incluir Professores</ModalHeader>
       <ModalBody>
         <div className="form-group">
-          <label>Nome do Aluno: </label>
+          <label>Nome do Professor: </label>
           <br />
           <input type="text" className="form-control" name="nomeAluno"  onChange={handleChange}/>
           <br />
-          <label>Nome do Responsável: </label>
+          <label>Atividade Responsável: </label>
+          <input type="text" className="form-control" name="atividadeResponsavel" onChange={handleChange}/>
+
           <br />
-          <input type="text" className="form-control" name="nomeResponsavel" onChange={handleChange}/>
-          <br />
-          <label>Idade: </label>
-          <br />
-          <input type="text" className="form-control" name="idade" onChange={handleChange}/>
-          <br />
-          <label>RG: </label>
-          <br />
-          <input type="text" className="form-control" name="rg" onChange={handleChange}/>
-          <br />
-          <label>CPF: </label>
-          <br />
-          <input type="text" className="form-control" name="cpf" onChange={handleChange}/>
-          <br />
+      
         </div>
       </ModalBody>
       <ModalFooter>
@@ -191,32 +168,17 @@ function App() {
     </Modal>
 
     <Modal isOpen={modalEditar}>
-      <ModalHeader>Editar Aluno</ModalHeader>
+      <ModalHeader>Editar Professor</ModalHeader>
       <ModalBody>
         <div className="form-group">
         <label>ID: </label>
-          <br />
-          <input type="text" className="form-control" readOnly value={alunoSelecionado && alunoSelecionado.id}/>
-          <br />
-          <label>Nome do Aluno: </label>
+        <label>Nome do Professor: </label>
           <br />
           <input type="text" className="form-control" name="nomeAluno"  onChange={handleChange}/>
           <br />
-          <label>Nome do Responsável: </label>
-          <br />
-          <input type="text" className="form-control" name="nomeResponsavel" onChange={handleChange}/>
-          <br />
-          <label>Idade: </label>
-          <br />
-          <input type="text" className="form-control" name="idade" onChange={handleChange}/>
-          <br />
-          <label>RG: </label>
-          <br />
-          <input type="text" className="form-control" name="rg" onChange={handleChange}/>
-          <br />
-          <label>CPF: </label>
-          <br />
-          <input type="text" className="form-control" name="cpf" onChange={handleChange}/>
+          <label>Atividade Responsável: </label>
+          <input type="text" className="form-control" name="atividadeResponsavel" onChange={handleChange}/>
+
           <br />
         </div>
       </ModalBody>
@@ -228,7 +190,7 @@ function App() {
 
     <Modal isOpen={modalExcluir}>
         <ModalBody>
-        Confirma a exclusão deste(a) aluno(a) : {alunoSelecionado && alunoSelecionado.nomeAluno} ?
+        Confirma a exclusão deste(a) professor(a) : {professorSelecionado && professorSelecionado.nomeProfessor} ?
         </ModalBody>
         <ModalFooter>
           <button className="btn btn-danger" onClick={()=>pedidoDelete()}>
@@ -241,10 +203,12 @@ function App() {
           </button>
         </ModalFooter>
       </Modal>
-    
-         
+    <br/><br/>
+          <h6>RA: 2101477</h6>
+          <h6>Nome: Júlia Reis de Aguiar</h6>
+          <h6>5ºSemestre-ADS</h6>
     </div>
   );
 }
 
-export default App;
+export default Teacher;
--- FRONT/src/index.js
+++ FRONT/src/index.js
@@ -2,10 +2,12 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
+import Teacher from './Teacher';
 
 ReactDOM.render(
   <React.StrictMode>
     <App />
+    <Teacher />
   </React.StrictMode>,
   document.getElementById('root')
 );
